d3df36287488014be0639491d1745fc532a00281
Jesse Watson <jexxe@live.com>
2024-04-16 10:24:06 -0400

    fix5

--- frontend/src/Axios/Axios.tsx
+++ frontend/src/Axios/Axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5045",
+  baseURL: "http://deposticalc.com",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
