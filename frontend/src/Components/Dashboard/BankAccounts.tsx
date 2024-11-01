3831019c3ed86b62e569facb117242e2a4d3e3ba
Jesse Watson <jexxe@live.com>
2024-11-01 18:49:37 -0400

    remove balance, serves no purpose

--- frontend/src/Components/Dashboard/BankAccounts.tsx
+++ frontend/src/Components/Dashboard/BankAccounts.tsx
@@ -76,32 +76,32 @@ const BankAccounts = (props: Props) => {
         return bankTypes[params.value];
       },
     },
-    {
-      field: "amount",
-      headerName: "Balance",
-      flex: 1,
-      description:
-        "Balance is the amount of expenses minus income into the account.",
-      renderCell: (params) => {
-        const calcColor = (value: string) => {
-          const floatValue = parseFloat(value);
-          if (floatValue < 0) return "#D48181"; // "error"
-          if (floatValue > 0) return "#66bb6a"; // "success"
-          return "";
-        };
-        return (
-          <Stack spacing={1} direction="row" height="100%" alignItems="center">
-            <Typography>$</Typography>
-            <Typography color={calcColor(params.value)}>
-              {params.value.toLocaleString(undefined, {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}
-            </Typography>
-          </Stack>
-        );
-      },
-    },
+    // {
+    //   field: "amount",
+    //   headerName: "Balance",
+    //   flex: 1,
+    //   description:
+    //     "Balance is the amount of expenses minus income into the account.",
+    //   renderCell: (params) => {
+    //     const calcColor = (value: string) => {
+    //       const floatValue = parseFloat(value);
+    //       if (floatValue < 0) return "#D48181"; // "error"
+    //       if (floatValue > 0) return "#66bb6a"; // "success"
+    //       return "";
+    //     };
+    //     return (
+    //       <Stack spacing={1} direction="row" height="100%" alignItems="center">
+    //         <Typography>$</Typography>
+    //         <Typography color={calcColor(params.value)}>
+    //           {params.value.toLocaleString(undefined, {
+    //             minimumFractionDigits: 2,
+    //             maximumFractionDigits: 2,
+    //           })}
+    //         </Typography>
+    //       </Stack>
+    //     );
+    //   },
+    // },
     {
       field: "bank",
       headerName: "",
