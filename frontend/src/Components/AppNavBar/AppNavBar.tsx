ba4bb493019bfe559dfbff0ffa7184b63438f672
Jesse Watson <jexxe@live.com>
2024-11-03 07:31:55 -0500

    chore - changes for package 1.7 -> 2.1

--- frontend/src/Components/AppNavBar/AppNavBar.tsx
+++ frontend/src/Components/AppNavBar/AppNavBar.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Transition, DisclosureButton, MenuButton, MenuItems, MenuItem, DisclosurePanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [{ name: "Dashboard", href: "#", current: true }];
+const navigation = [{ name: "Dashboard", href: "/dashboard", current: true }];
 const userNavigation = [
-  { name: "Your Profile", href: "/profile" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "/logout" },
+  { label: "Profile", href: "/profile" },
+  { label: "Settings", href: "/settings" },
+  { label: "Sign out", href: "/logout" },
 ];
 
 function classNames(...classes: string[]) {
@@ -23,7 +23,7 @@ export default function AppNavBar() {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
                     {open ? (
@@ -31,7 +31,7 @@ export default function AppNavBar() {
                     ) : (
                       <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                     )}
-                  </Disclosure.Button>
+                  </DisclosureButton>
                 </div>
                 <div className="flex flex-shrink-0 items-center">
                   {/*
@@ -74,7 +74,7 @@ export default function AppNavBar() {
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
-                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100">
                           <svg
                             className="h-full w-full text-gray-300"
@@ -84,7 +84,7 @@ export default function AppNavBar() {
                             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                           </svg>
                         </span>
-                      </Menu.Button>
+                      </MenuButton>
                     </div>
                     <Transition
                       as={Fragment}
@@ -95,23 +95,23 @@ export default function AppNavBar() {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-[#e1f1f5] py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-[#e1f1f5] py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
+                          <MenuItem as={Fragment} key={item.label}>
+                            {({ focus }) => (
                               <a
                                 href={item.href}
                                 className={classNames(
-                                  active ? "bg-[rgba(0,0,0,0.04)]" : "",
+                                  focus ? "bg-[rgba(0,0,0,0.04)]" : "",
                                   "block px-4 py-2 text-sm text-[#1e5d81]",
                                 )}
                               >
-                                {item.name}
+                                {item.label}
                               </a>
                             )}
-                          </Menu.Item>
+                          </MenuItem>
                         ))}
-                      </Menu.Items>
+                      </MenuItems>
                     </Transition>
                   </Menu>
                 </div>
@@ -119,10 +119,10 @@ export default function AppNavBar() {
             </div>
           </div>
 
-          <Disclosure.Panel className="md:hidden">
+          <DisclosurePanel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <DisclosureButton
                   key={item.name}
                   as="a"
                   href={item.href}
@@ -135,24 +135,24 @@ export default function AppNavBar() {
                   aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </DisclosureButton>
               ))}
             </div>
             <div className="border-t border-gray-700 pb-3 pt-4">
               <div className="space-y-1 px-2 sm:px-3">
                 {userNavigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
+                  <DisclosureButton
+                    key={item.label}
                     as="a"
                     href={item.href}
                     className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-[#247fca] hover:text-white"
                   >
-                    {item.name}
-                  </Disclosure.Button>
+                    {item.label}
+                  </DisclosureButton>
                 ))}
               </div>
             </div>
-          </Disclosure.Panel>
+          </DisclosurePanel>
         </>
       )}
     </Disclosure>
