91a4bd8b5d7295b66cc4a1018e178a4e1ef3c8c5
Jesse Watson <jexxe@live.com>
2024-11-02 21:28:40 -0400

    pie chart for future use

--- frontend/src/Components/Dashboard/Components/PieChart.tsx
+++ frontend/src/Components/Dashboard/Components/PieChart.tsx
@@ -3,6 +3,7 @@ import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
 import { useEffect, useState } from 'react';
 import { expenseFrequencyMap, payFrequencyMap } from '@Utils';
 import { Paper, Stack, Typography } from '@mui/material';
+import { Portfolio } from '@/Utils/Models/Portfolio';
 payFrequencyMap
 
 type PieChartData = {
@@ -14,56 +15,119 @@ type PieChartData = {
 const PieCharts = () => {
   const portfolio = useAppSelector((state) => state.portfolio);
   const [incomeData, setIncomeData] = useState<PieChartData[]>([])
-  const [expenseData, setExpenseData] = useState([])
-
+  const [expenseData, setExpenseData] = useState<PieChartData[]>([])
+  const [totalAmount, setTotalAmount] = useState<number>(0);
 
   // Generate Pie data
   useEffect(() => {
-    const generateIncomePieChartData = () => {
-      let totalIncome = 0;
-      const incomes = portfolio.incomes;
-      for (const i in incomes) {
-        const income = portfolio.incomes[i]
-        totalIncome += (income.amount + income.insuranceAmount) * payFrequencyMap[income.payFrequency]
-      }
+    const port = new Portfolio(portfolio)
 
-      const data = incomes.map((income, index) => {
-        const incomePercentage = ((((income.amount + income.insuranceAmount) * payFrequencyMap[income.payFrequency]) / totalIncome) * 100).toFixed(1)
-        return {
-          id: index,
-          value: Number(incomePercentage),
+    const generateIncomePieChartData = () => {
+      const data: PieChartData[] = [];
+      for (const income of port.incomes) {
+        const percentage = port.getPercentageOfIncome(income)
+        data.push({
+          id: income.id,
+          value: percentage,
           label: income.name
-        }
-      })
+        })
+      }
       setIncomeData(data)
     }
 
-    const generateExpensePieChartData = () => {
-      let totalAnnualExpenses = 0;
-      portfolio.expenses.forEach((expense) => {
-        console.log(`Expense: ${expense.name} - ${((expense.amount * expenseFrequencyMap[expense.frequency]) / 12).toFixed(2)}`)
-        const amount = expense.amount * expenseFrequencyMap[expense.frequency];
-        totalAnnualExpenses += amount;
-        console.log(`Total: ${(totalAnnualExpenses / 12).toFixed(2)}`)
+    // const generateBankPieChartData = () => {
+    //   const data: PieChartData[] = [];
+    //   for (const bank of port.bankAccounts) {
+    //     if (bank.isRemainder) continue;
+    //     if (bank.type === 1) continue;
+    //     const percentage = port.getBanksPercentageOfTotalExpenses(bank)
+    //     data.push({
+    //       id: bank.id,
+    //       value: percentage,
+    //       label: bank.name
+    //     })
+    //   }
+    //   setExpenseData(data)
+    // }
+
+
+    const generateBankPieChartData = () => {
+      let totalAmountPerMonth = 0;
+      const data: PieChartData[] = [];
+      for (const bank of port.bankAccounts) {
+        if (bank.isRemainder) continue;
+        // handle savings
+        if (bank.type === 1) {
+          if (bank.isPercentage) {
+            const annualIncome = port.getTotalIncomeAnnually();
+            const annualSavingsForAccount = (annualIncome * (bank.percentageAmount / 100))
+            const amount = annualSavingsForAccount / 12
+            totalAmountPerMonth += Number(amount.toFixed(2))
+            data.push({
+              id: bank.id,
+              value: Number(amount.toFixed(2)),
+              label: bank.name
+            })
+          }
+          if (!bank.isPercentage && bank.amount !== 0) {
+            totalAmountPerMonth += Number(bank.amount.toFixed(2))
+            data.push({
+              id: bank.id,
+              value: bank.amount,
+              label: bank.name
+            })
+          }
+        }
+        if (bank.type === 0) {
+
+          let bankExpenses = 0;
+          for (const expense of bank.expenses) {
+            const annualExpenseAmount = Number((expense.amount * expenseFrequencyMap[expense.frequency]).toFixed(2))
+            bankExpenses += annualExpenseAmount;
+          }
+          const monthlyExpenses = Number((bankExpenses / 12).toFixed(2))
+          if (monthlyExpenses === 0) continue;
+          totalAmountPerMonth += monthlyExpenses;
+          data.push({
+            id: bank.id,
+            value: monthlyExpenses,
+            label: bank.name
+          })
+        }
+      }
+
+      const remainingAmount = port.getTotalIncomeMonthly() - totalAmountPerMonth
+      totalAmountPerMonth += remainingAmount;
+
+      data.push({
+        id: 999,
+        value: remainingAmount,
+        label: "Remaining"
       })
 
+      setTotalAmount(Number(totalAmountPerMonth.toFixed(2)))
+      setExpenseData(data)
     }
-    generateIncomePieChartData();
-    generateExpensePieChartData();
+
+
+    if (portfolio.incomes.length > 0)
+      generateIncomePieChartData();
+    if (portfolio.bankAccounts.length > 0)
+      generateBankPieChartData();
 
   }, [portfolio])
 
   return (
     <Paper sx={{ p: 2 }}>
       <Typography variant='h6'>Data</Typography>
-      {incomeData.length > 0 &&
-        <Stack direction="row">
+      <Stack direction="column" spacing={3}>
+        {incomeData.length > 0 &&
           <PieChart
             series={[
               {
                 arcLabel: (item) => `${item.value}%`,
                 arcLabelMinAngle: 35,
-                arcLabelRadius: '50%',
+                arcLabelRadius: '60%',
                 highlightScope: {
                   fade: 'global',
                   highlight: 'item'
@@ -81,19 +145,21 @@ const PieCharts = () => {
                 fontWeight: 'bold',
               },
             }}
-            slotProps={{
-              legend: {
-                hidden: true
-              }
-            }}
-            height={200}
-          />
+            // slotProps={{
+            //   legend: {
+            //     hidden: true
+            //   }
+            // }}
+            height={300}
+          />}
+
+        {expenseData.length > 0 &&
           <PieChart
             series={[
               {
-                arcLabel: (item) => `${item.value}%`,
+                arcLabel: (item) => `${((item.value / totalAmount) * 100).toFixed(2)}%`,
                 arcLabelMinAngle: 35,
-                arcLabelRadius: '50%',
+                arcLabelRadius: '60%',
                 highlightScope: {
                   fade: 'global',
                   highlight: 'item'
@@ -103,7 +169,7 @@ const PieCharts = () => {
                   additionalRadius: -30,
                   color: 'gray'
                 },
-                data: incomeData
+                data: expenseData
               },
             ]}
             sx={{
@@ -111,9 +177,9 @@ const PieCharts = () => {
                 fontWeight: 'bold',
               },
             }}
-            height={200}
-          />
-        </Stack>}
+            height={300}
+          />}
+      </Stack>
     </Paper>
   );
 }
