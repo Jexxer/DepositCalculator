1e91d5158d1317c4a679df09b68a9dcf8d02eb83
Jesse Watson <jexxe@live.com>
2024-11-03 21:06:30 -0500

    re-add totals for testing UI

--- frontend/src/Components/Dashboard/BankAccounts.tsx
+++ frontend/src/Components/Dashboard/BankAccounts.tsx
@@ -19,6 +19,7 @@ import { useState } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import MoreVert from "@mui/icons-material/MoreVert";
 import EditBankAccountForm from "./Components/EditBankAccountForm";
+import { Portfolio } from "@/Utils/Models/Portfolio";
 
 type Props = {
   paperProps: PaperProps;
@@ -29,9 +30,8 @@ const bankTypes = ["Checking", "Savings"];
 const BankAccounts = (props: Props) => {
   const dispatch = useAppDispatch();
   const { paperProps } = props;
-  const { bankAccounts, incomes, expenses } = useAppSelector(
-    (state) => state.portfolio,
-  );
+  const portfolio = useAppSelector((state) => state.portfolio);
+  const { bankAccounts, incomes, expenses } = portfolio;
   const [addOpen, setAddOpen] = useState<boolean>(false);
   const [editOpen, setEditOpen] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -76,32 +76,50 @@ const BankAccounts = (props: Props) => {
         return bankTypes[params.value];
       },
     },
-    // {
-    //   field: "amount",
-    //   headerName: "Balance",
-    //   flex: 1,
-    //   description:
-    //     "Balance is the amount of expenses minus income into the account.",
-    //   renderCell: (params) => {
-    //     const calcColor = (value: string) => {
-    //       const floatValue = parseFloat(value);
-    //       if (floatValue < 0) return "#D48181"; // "error"
-    //       if (floatValue > 0) return "#66bb6a"; // "success"
-    //       return "";
-    //     };
-    //     return (
-    //       <Stack spacing={1} direction="row" height="100%" alignItems="center">
-    //         <Typography>$</Typography>
-    //         <Typography color={calcColor(params.value)}>
-    //           {params.value.toLocaleString(undefined, {
-    //             minimumFractionDigits: 2,
-    //             maximumFractionDigits: 2,
-    //           })}
-    //         </Typography>
-    //       </Stack>
-    //     );
-    //   },
-    // },
+    {
+      field: "amount",
+      headerName: "Totals",
+      flex: 1,
+      description:
+        "Balance is the amount of expenses minus income into the account.",
+      renderCell: (params) => {
+        const calcColor = (value: number) => {
+          if (value < 0) return "#D48181"; // "error"
+          if (value > 0) return "#66bb6a"; // "success"
+          return "";
+        };
+        let amount = 0;
+        const port = new Portfolio(portfolio);
+        const bank = port.bankAccounts.find((b => b.id === params.row.bank.id))
+        if (params.row.bank.type === 0) { // if checking account
+          if (bank) {
+            amount = bank?.getTotalExpensesMonthly();
+          }
+        }
+        if (params.row.bank.type === 1) { // if savings account
+          if (bank) {
+            if (bank.isPercentage) {
+              const income = port.getTotalIncomeMonthly();
+              amount = Number((income * (bank.percentageAmount / 100)).toFixed(2))
+            }
+          }
+
+        }
+
+
+        return (
+          <Stack spacing={1} direction="row" height="100%" alignItems="center">
+            <Typography>$</Typography>
+            <Typography color={calcColor(amount)}>
+              {amount.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </Typography>
+          </Stack>
+        );
+      },
+    },
     {
       field: "bank",
       headerName: "",
@@ -187,7 +205,7 @@ const BankAccounts = (props: Props) => {
       name: b.name,
       type: b.type,
       isRemainer: b.isRemainder,
-      amount: amount,
+      amount: b,
       bank: b,
     };
   });
@@ -213,21 +231,22 @@ const BankAccounts = (props: Props) => {
             <AddIcon />
           </IconButton>
         </Stack>
-        <DataGrid
-          autoHeight
-          hideFooter
-          sx={{
-            borderRadius: "unset",
-          }}
-          rows={rows}
-          columns={columns}
-          disableRowSelectionOnClick
-          getRowClassName={(params) =>
-            params.indexRelativeToCurrentPage % 2 === 0
-              ? "even"
-              : "odd-data-grid-row"
-          }
-        />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <DataGrid
+            hideFooter
+            sx={{
+              borderRadius: "unset",
+            }}
+            rows={rows}
+            columns={columns}
+            disableRowSelectionOnClick
+            getRowClassName={(params) =>
+              params.indexRelativeToCurrentPage % 2 === 0
+                ? "even"
+                : "odd-data-grid-row"
+            }
+          />
+        </div>
       </Paper>
 
       <Modal open={addOpen} onClose={() => setAddOpen(false)}>
